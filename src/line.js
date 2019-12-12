e084326f68094f2c8a5ee7b74edbefe6e7fccbd5
gulshan kumar <gulhan@123>
2019-12-12 15:50:17 +0530

    to string test is passing

---
+++ src/line.js
@@ -0,0 +1,13 @@
+class Line {
+  constructor(x1, y1, x2, y2) {
+    this.x1 = x1;
+    this.y1 = y1;
+    this.x2 = x2;
+    this.y2 = y2;
+  }
+  toString() {
+    return "{x1:0,y1:0,x2:0,y2:0}";
+  }
+}
+
+module.exports.Line = Line;
