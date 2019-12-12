89ca89289a2e236c77638f7a758701a04573caad
gulshan kumar <gulhan@123>
2019-12-12 16:10:33 +0530

    isEqual is passing

--- src/line.js
+++ src/line.js
@@ -6,7 +6,14 @@ class Line {
     this.y2 = y2;
   }
   toString() {
-    return "{x1:0,y1:0,x2:0,y2:0}";
+    return `{x1:${this.x1},y1:${this.y1},x2:${this.x2},y2:${this.y2}}`;
+  }
+  isEqual(otherLine) {
+    const isX1Equal = this.x1 == otherLine.x1;
+    const isY1Equal = this.y1 == otherLine.y1;
+    const isX2Equal = this.x2 == otherLine.x2;
+    const isY2Equal = this.y2 == otherLine.y2;
+    return isX1Equal && isY1Equal && isX2Equal && isY2Equal;
   }
 }
 
