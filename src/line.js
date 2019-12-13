48fa09d3696567480dbe7de64b8946768cfbab2e
gulshan kumar <gulhan@123>
2019-12-13 08:27:28 +0530

    added isTypeEqual function to the line.js

--- src/line.js
+++ src/line.js
@@ -2,6 +2,10 @@ const arePointsEqual = function(point1, point2) {
   return point1.x == point2.x && point1.y == point2.y;
 };
 
+const isTypeEqual = function(line1, line2) {
+  return line1 instanceof Line && line2 instanceof Line;
+};
+
 class Line {
   constructor(endA, endB) {
     this.endA = endA;
@@ -11,10 +15,10 @@ class Line {
     return `line:(${this.endA.x},${this.endA.y}),(${this.endB.x},${this.endB.y})`;
   }
   isEqual(otherLine) {
-    const isTypeEqual = this instanceof Line && otherLine instanceof Line;
+    const isTypeOfBothLineEqual = isTypeEqual(this, otherLine);
     const areEndAEqual = arePointsEqual(this.endA, otherLine.endA);
     const areEndBEqual = arePointsEqual(this.endB, otherLine.endB);
-    return areEndAEqual && areEndBEqual && isTypeEqual;
+    return areEndAEqual && areEndBEqual && isTypeOfBothLineEqual;
   }
 }
 
