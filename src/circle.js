7d6bedb37944cf0165ad8fcad93fcf975afa5fff
gulshan kumar <gulhan@123>
2019-12-16 22:24:00 +0530

    Added isEqualTo methos to the class Circle

--- src/circle.js
+++ src/circle.js
@@ -6,6 +6,14 @@ class Circle {
   toString() {
     return `[Circle @(${this.centre.x},${this.centre.y}) radius ${this.radius}]`;
   }
+  isEqualTo(otherCircle) {
+    if (this === otherCircle) return true;
+    if (!(otherCircle instanceof Circle)) return false;
+    return (
+      this.radius == otherCircle.radius &&
+      this.centre.isEqualTo(otherCircle.centre)
+    );
+  }
 }
 
 module.exports.Circle = Circle;
--- test/testCircle.js
+++ test/testCircle.js
@@ -13,4 +13,35 @@ describe("Circle", () => {
       assert.strictEqual(actual, expected);
     });
   });
+  describe("isEqualTo", () => {
+    it("should validate if both the circles have same reference", () => {
+      const circle = new Circle(new Point(1, 2), 5);
+      const actual = circle.isEqualTo(circle);
+      assert.isOk(actual);
+    });
+    it("should invalidate if both the circles have not same instance", () => {
+      const circle1 = new Circle(new Point(1, 2), 5);
+      const circle2 = { centre: { x: 1, y: 2 }, radius: 5 };
+      const actual = circle1.isEqualTo(circle2);
+      assert.isNotOk(actual);
+    });
+    it("should validate the circle which have same instance and fields values", () => {
+      const circle1 = new Circle(new Point(1, 2), 5);
+      const circle2 = new Circle(new Point(1, 2), 5);
+      const actual = circle1.isEqualTo(circle2);
+      assert.isOk(actual);
+    });
+    it("should invalidate the circle which have different centres", () => {
+      const circle1 = new Circle(new Point(1, 2), 5);
+      const circle2 = new Circle(new Point(1, 5), 5);
+      const actual = circle1.isEqualTo(circle2);
+      assert.isNotOk(actual);
+    });
+    it("should invalidate the circle which have different radius", () => {
+      const circle1 = new Circle(new Point(1, 2), 5);
+      const circle2 = new Circle(new Point(1, 2), 4);
+      const actual = circle1.isEqualTo(circle2);
+      assert.isNotOk(actual);
+    });
+  });
 });
